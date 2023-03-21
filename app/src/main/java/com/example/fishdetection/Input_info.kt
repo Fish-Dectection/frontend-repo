f72148f6056c98dd0b20a961ab882568e03e9726
DongWon <ehddnjs1747@daum.net>
2023-03-21 15:34:27 +0900

    start project

---
+++ app/src/main/java/com/example/fishdetection/Input_info.kt
@@ -0,0 +1,11 @@
+package com.example.fishdetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Input_info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_info)
+    }
+}
