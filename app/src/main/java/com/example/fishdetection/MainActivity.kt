e5ecdac2beb16937b53199fe74498292f8367ad6
DongWon <ehddnjs1747@daum.net>
2023-03-26 19:22:46 +0900

    main 코드 작성

--- app/src/main/java/com/example/fishdetection/MainActivity.kt
+++ app/src/main/java/com/example/fishdetection/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.fishdetection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fishdetection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var main_binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main_binding.root)
+
+        main_binding.buttno1.setOnClickListener{
+            val input_intent = Intent(this,Input_info::class.java)
+            startActivity(input_intent)
+
+        }
     }
 }
